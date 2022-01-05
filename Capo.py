66e3a5e4a283da39e0c88a1240a07ca519f88bff
0xdea1yn <32786549+Toyuri453@users.noreply.github.com>
2022-01-06 00:03:59 +0800

    Add CDF graph

--- Capo.py
+++ Capo.py
@@ -9,17 +9,24 @@ import GenNorm
 import Terminal
 import AxesFrame
 from matplotlib import rcParams
+import pandas as pd
+
 rcParams.update({'figure.autolayout': True})
 
-type_factor_sigma = {'UWB' : 0, 'BlueTooth' : 0.3}
+type_factor_sigma = {'UWB' : 0, 'BlueTooth' : 5}
 
-t_a = Terminal.CartesianPoint(0, 0, "UWB", "TerminalA")
-t_b = Terminal.CartesianPoint(15, 4, "UWB", "TerminalB")
-t_c = Terminal.CartesianPoint(5, 11, "UWB", "TerminalC")
-t_d = Terminal.CartesianPoint(10, 9, "UWB", "TerminalD")
-t_e = Terminal.CartesianPoint(8, 10, "UWB", "TerminalE")
-t_f = Terminal.CartesianPoint(-4, 3, "UWB", "TerminalF")
-t_o = Terminal.CartesianPoint(4, 4, "BlueTooth", "Target")
+font1 = {
+'weight' : 'normal',
+'size'   : 12,
+}
+
+t_a = Terminal.CartesianPoint(100, 100, "UWB", "TerminalA")
+t_b = Terminal.CartesianPoint(-130, 20, "UWB", "TerminalB")
+t_c = Terminal.CartesianPoint(40, -80, "UWB", "TerminalC")
+t_d = Terminal.CartesianPoint(120, 90, "UWB", "TerminalD")
+t_e = Terminal.CartesianPoint(80, 100, "UWB", "TerminalE")
+t_f = Terminal.CartesianPoint(-40, 30, "UWB", "TerminalF")
+t_o = Terminal.CartesianPoint(5, 5, "BlueTooth", "Target")
 
 def get_rho(terminal, target):
     dis = Calculation.get_distance(terminal, target)
@@ -37,6 +44,7 @@ def LSM(t_a: 'Terminal.CartesianPoint', t_b: 'Terminal.CartesianPoint', t_c: 'Te
     e_d_b = np.random.normal(math.sqrt(d_b), get_rho(t_b, t_o))
     e_d_c = np.random.normal(math.sqrt(d_c), get_rho(t_c, t_o))
 
+    print(e_d_a, e_d_b, e_d_c)
 
     m_a = np.array([[2*(t_a._x-t_c._x), 2*(t_a._y-t_c._y)], [2*(t_a._x-t_b._x), 2*(t_a._y-t_b._y)], [2*(t_b._x-t_c._x), 2*(t_b._y-t_c._y)]])
     m_aT = m_a.T
@@ -61,7 +69,7 @@ def LSM(t_a: 'Terminal.CartesianPoint', t_b: 'Terminal.CartesianPoint', t_c: 'Te
     d_b = Calculation.get_distance(t_b, t_o)
     d_c = Calculation.get_distance(t_c, t_o)
 
-    return abs(a_op_dis-d_a), abs(e_d_a-d_a), abs(b_op_dis-d_b), abs(e_d_b-d_b), abs(c_op_dis-d_c), abs(e_d_c-d_c)
+    return abs(a_op_dis-d_a), abs(e_d_a-d_a), abs(b_op_dis-d_b), abs(e_d_b-d_b), abs(c_op_dis-d_c), abs(e_d_c-d_c), (abs(a_op_dis-d_a)+abs(b_op_dis-d_b)+abs(c_op_dis-d_c))/3, (abs(e_d_a-d_a)+abs(e_d_b-d_b)+abs(e_d_c-d_c))/3
 
 def LSM_4(t_a: 'Terminal.CartesianPoint', t_b: 'Terminal.CartesianPoint', t_c: 'Terminal.CartesianPoint', t_d: 'Terminal.CartesianPoint', t_o: 'Terminal.CartesianPoint'):
     d_a = Calculation.get_distance_sq(t_a, t_o)
@@ -219,12 +227,24 @@ e_d_b_set = np.zeros((3000,1))
 c_op_dis_set = np.zeros((3000,1))
 e_d_c_set = np.zeros((3000,1))
 
+op_set = np.zeros((3000,1))
+avr_d = np.zeros((3000,1))
+
 a_sum = 0
 b_sum = 0
 c_sum = 0
 e_a_sum = 0
 e_b_sum = 0
 e_c_sum = 0
+
+data1 = []
+data2 = []
+data3 = []
+data4 = []
+data5 = []
+data6 = []
+data7 = []
+data8 = []
 
 for _ in range(3000):
     result = LSM(t_a, t_b, t_c, t_o)
@@ -234,18 +254,30 @@ for _ in range(3000):
     e_d_b_set[_] = result[3]
     c_op_dis_set[_] = result[4]
     e_d_c_set[_] = result[5]
+    op_set[_] = result[6]
+    avr_d[_] = result[7]
     a_sum += a_op_dis_set[_]
     b_sum += b_op_dis_set[_]
     c_sum += c_op_dis_set[_]
     e_a_sum += e_d_a_set[_]
     e_b_sum += e_d_b_set[_]
     e_c_sum += e_d_c_set[_]
+    data1.append(result[1])
+    data2.append(result[3])
+    data3.append(result[5])
+    data4.append(result[0])
+    data5.append(result[2])
+    data6.append(result[4])
+    data7.append(result[6])
+    data8.append(result[7])
     print (result)
 
 fig, (ax1) = plt.subplots(1,1, figsize=(5,3))
 fig2, (ax2) = plt.subplots(1,1, figsize=(5,3))
 fig3, (ax3) = plt.subplots(1,1, figsize=(5,3))
 fig4, (ax4) = plt.subplots(1,1, figsize=(5,3))
+fig5, (ax5) = plt.subplots(1,1, figsize=(5,3))
+fig6, ((ax6, ax7, ax8, ax9)) = plt.subplots(1,4,figsize=(19,4))
 
 ax1.set_xlabel("Error distance", fontsize=14)
 ax1.set_ylabel("Frequency", fontsize=14)
@@ -253,12 +285,25 @@ ax1.set_ylabel("Frequency", fontsize=14)
 n, bins, patches = ax1.hist(e_d_a_set, bins=80, facecolor='#bf1722',
                            edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='original')
 n, bins, patches = ax1.hist(a_op_dis_set, bins=80, facecolor='#17aebf',
-                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='CAPO')
+                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='Capo')
 
 n = n.astype('int')
 ax1.legend(fontsize = 12)
 ax1.xaxis.grid(True, which='major', linestyle=(0, (8, 4)))
 ax1.yaxis.grid(True, which='major', linestyle=(0, (8, 4)))
+
+ax5.set_xlabel("Error distance", fontsize=14)
+ax5.set_ylabel("Frequency", fontsize=14)
+#ax1.set_title('Error distribution of Terminal A', fontsize=15, fontweight='light')
+n, bins, patches = ax5.hist(avr_d, bins=80, facecolor='#bf1722',
+                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='original')
+n, bins, patches = ax5.hist(op_set, bins=80, facecolor='#17aebf',
+                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='Capo')
+
+n = n.astype('int')
+ax5.legend(fontsize = 12)
+ax5.xaxis.grid(True, which='major', linestyle=(0, (8, 4)))
+ax5.yaxis.grid(True, which='major', linestyle=(0, (8, 4)))
 
 ax2.set_xlabel("Error distance", fontsize=14)
 ax2.set_ylabel("Frequency", fontsize=14)
@@ -266,7 +311,7 @@ ax2.set_ylabel("Frequency", fontsize=14)
 n, bins, patches = ax2.hist(e_d_b_set, bins=80, facecolor='#bf1722',
                            edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='original')
 n, bins, patches = ax2.hist(b_op_dis_set, bins=80, facecolor='#17aebf',
-                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='CAPO')
+                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='Capo')
 
 n = n.astype('int')
 ax2.legend(fontsize = 12)
@@ -279,7 +324,7 @@ ax3.set_ylabel("Frequency", fontsize=14)
 n, bins, patches = ax3.hist(e_d_c_set, bins=80, facecolor='#bf1722',
                            edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='original')
 n, bins, patches = ax3.hist(c_op_dis_set, bins=80, facecolor='#17aebf',
-                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='CAPO')
+                           edgecolor='#000000',histtype='stepfilled', linewidth=0.5, alpha=0.30, label='Capo')
 
 n = n.astype('int')
 ax3.legend(fontsize = 12)
@@ -319,10 +364,10 @@ for _ in range(3000):
     a5_error += (r5[0]+r5[2]+r5[4]+r5[6]+r5[8])/5
     a6_error += (r6[0]+r6[2]+r6[4]+r6[6]+r6[8]+r6[10])/6
     if _ == 9 :
-        ax4.scatter(_, a3_error / 10, color = '#bf1722', linewidths=[1], label = "3 terminals")
-        ax4.scatter(_, a4_error / 10, color='#17aebf', linewidths=[1], marker = "^", label = "4 terminals")
-        ax4.scatter(_, a5_error / 10, color='#57ba06', linewidths=[1], marker = "s", label = "5 terminals")
-        ax4.scatter(_, a6_error / 10, color='#8d17bf', linewidths=[1], marker = "x", label = "6 terminals")
+        ax4.scatter(_, a3_error / 10, color = '#bf1722', linewidths=[1], label = "3 devices")
+        ax4.scatter(_, a4_error / 10, color='#17aebf', linewidths=[1], marker = "^", label = "4 devices")
+        ax4.scatter(_, a5_error / 10, color='#57ba06', linewidths=[1], marker = "s", label = "5 devices")
+        ax4.scatter(_, a6_error / 10, color='#8d17bf', linewidths=[1], marker = "x", label = "6 devices")
         round_list.append(_)
         a3_error_list.append(a3_error / 10)
         a4_error_list.append(a4_error / 10)
@@ -405,7 +450,122 @@ ax4.plot(round_list, a4_error_list, c = '#17aebf', linewidth = 0.8)
 ax4.plot(round_list, a5_error_list, c = '#57ba06', linewidth = 0.8)
 ax4.plot(round_list, a6_error_list, c = '#8d17bf', linewidth = 0.8)
 ax4.legend(fontsize = 10,framealpha=0)
+
+#BLE
+print(data1)
+denominator=len(data1)#分母数量 e_d_a-d_a r[1] e_d_a_set
+Data1=pd.Series(data1)#将数据转换为Series利用分组频数计算
+Fre1=Data1.value_counts()
+Fre1_sort=Fre1.sort_index(axis=0,ascending=True)
+Fre1_df=Fre1_sort.reset_index()#将Series数据转换为DataFrame
+Fre1_df[0]=Fre1_df[0]/denominator#转换成概率
+Fre1_df.columns=['Rds','Fre1']
+Fre1_df['cumsum']=np.cumsum(Fre1_df['Fre1'])
+
+denominator=len(data2)
+Data2=pd.Series(data2)
+Fre2=Data2.value_counts()
+Fre2_sort=Fre2.sort_index(axis=0,ascending=True)
+Fre2_df=Fre2_sort.reset_index()#将Series数据转换为DataFrame
+Fre2_df[0]=Fre2_df[0]/denominator#转换成概率
+Fre2_df.columns=['Rds','Fre2']
+Fre2_df['cumsum']=np.cumsum(Fre2_df['Fre2'])
+
+denominator=len(data3)
+Data3=pd.Series(data3)
+Fre3=Data3.value_counts()
+Fre3_sort=Fre3.sort_index(axis=0,ascending=True)
+Fre3_df=Fre3_sort.reset_index()#将Series数据转换为DataFrame
+Fre3_df[0]=Fre3_df[0]/denominator#转换成概率
+Fre3_df.columns=['Rds','Fre3']
+Fre3_df['cumsum']=np.cumsum(Fre3_df['Fre3'])
+
+#Capo
+
+denominator=len(data4)#分母数量 r[0] a_op_dis_set
+Data4=pd.Series(data4)#将数据转换为Series利用分组频数计算
+Fre4=Data4.value_counts()
+Fre4_sort=Fre4.sort_index(axis=0,ascending=True)
+Fre4_df=Fre4_sort.reset_index()#将Series数据转换为DataFrame
+Fre4_df[0]=Fre4_df[0]/denominator#转换成概率
+Fre4_df.columns=['Rds','Fre4']
+Fre4_df['cumsum']=np.cumsum(Fre4_df['Fre4'])
+
+denominator=len(data5)#分母数量 r[2] b_op_dis_set
+Data5=pd.Series(data5)#将数据转换为Series利用分组频数计算
+Fre5=Data5.value_counts()
+Fre5_sort=Fre5.sort_index(axis=0,ascending=True)
+Fre5_df=Fre5_sort.reset_index()#将Series数据转换为DataFrame
+Fre5_df[0]=Fre5_df[0]/denominator#转换成概率
+Fre5_df.columns=['Rds','Fre5']
+Fre5_df['cumsum']=np.cumsum(Fre5_df['Fre5'])
+
+denominator=len(data6)#分母数量 r[4] c_op_dis_set
+Data6=pd.Series(data6)#将数据转换为Series利用分组频数计算
+Fre6=Data6.value_counts()
+Fre6_sort=Fre6.sort_index(axis=0,ascending=True)
+Fre6_df=Fre6_sort.reset_index()#将Series数据转换为DataFrame
+Fre6_df[0]=Fre6_df[0]/denominator#转换成概率
+Fre6_df.columns=['Rds','Fre6']
+Fre6_df['cumsum']=np.cumsum(Fre6_df['Fre6'])
+
+#Average
+
+denominator=len(data7)#分母数量 r[4] c_op_dis_set
+Data7=pd.Series(data7)#将数据转换为Series利用分组频数计算
+Fre7=Data7.value_counts()
+Fre7_sort=Fre7.sort_index(axis=0,ascending=True)
+Fre7_df=Fre7_sort.reset_index()#将Series数据转换为DataFrame
+Fre7_df[0]=Fre7_df[0]/denominator#转换成概率
+Fre7_df.columns=['Rds','Fre7']
+Fre7_df['cumsum']=np.cumsum(Fre7_df['Fre7'])
+
+denominator=len(data8)#分母数量 r[4] c_op_dis_set
+Data8=pd.Series(data8)#将数据转换为Series利用分组频数计算
+Fre8=Data8.value_counts()
+Fre8_sort=Fre8.sort_index(axis=0,ascending=True)
+Fre8_df=Fre8_sort.reset_index()#将Series数据转换为DataFrame
+Fre8_df[0]=Fre8_df[0]/denominator#转换成概率
+Fre8_df.columns=['Rds','Fre8']
+Fre8_df['cumsum']=np.cumsum(Fre8_df['Fre8'])
+
+ax6.plot(Fre1_df['Rds'],Fre1_df['cumsum'],linestyle='--', color='#6699ff',label='Original')
+ax6.plot(Fre4_df['Rds'],Fre4_df['cumsum'],color='#00cc33',label='Capo')
+#ax1.set_title("CDF")
+ax6.set_xlabel("OA: Ranging error (m)", font1)
+ax6.set_ylabel("Cumulative Probability", font1)
+ax6.set_xlim(0,25)
+plt.legend(fontsize = 12, loc = 'upper left',framealpha=0)
+
+ax7.plot(Fre2_df['Rds'],Fre2_df['cumsum'],linestyle='--', color='#6699ff',label='Original')
+ax7.plot(Fre5_df['Rds'],Fre5_df['cumsum'],color='#00cc33',label='Capo')
+#ax1.set_title("CDF")
+ax7.set_xlabel("OB: Ranging error (m)", font1)
+ax7.set_ylabel("Cumulative Probability", font1)
+ax7.set_xlim(0,25)
+plt.legend(fontsize = 12, loc = 'upper left',framealpha=0)
+
+
+ax8.plot(Fre3_df['Rds'],Fre3_df['cumsum'],linestyle='--', color='#6699ff',label='Original')
+ax8.plot(Fre6_df['Rds'],Fre6_df['cumsum'],color='#00cc33',label='Capo')
+#ax1.set_title("CDF")
+ax8.set_xlabel("OC: Ranging error (m)", font1)
+ax8.set_ylabel("Cumulative Probability", font1)
+ax8.set_xlim(0,25)
+plt.legend(fontsize = 12, loc = 'upper left',framealpha=0)
+
+ax9.plot(Fre8_df['Rds'],Fre8_df['cumsum'],linestyle='--', color='#6699ff',label='Original')
+ax9.plot(Fre7_df['Rds'],Fre7_df['cumsum'],color='#00cc33',label='Capo')
+#ax1.set_title("CDF")
+ax9.set_xlabel("OC: Ranging error (m)", font1)
+ax9.set_ylabel("Cumulative Probability", font1)
+ax9.set_xlim(0,25)
+plt.legend(fontsize = 12, loc = 'upper left',framealpha=0)
+
 print(a3_error_list[7], a4_error_list[7], a5_error_list[7], a6_error_list[7])
+print(t_o._x, t_o._y)
+
 plt.show()
+
 
 
